8766b42194518990cda72ee6c01a076d148fe3bb
Makayla Jameson <makayla.jameson01@gmail.com>
2023-07-31 22:22:38 -0500

    created restaurant routes

--- app/api/restaurant_routes.py
+++ app/api/restaurant_routes.py
@@ -1,5 +1,17 @@
-# from flask import Blueprint
-# from app.models import Restaurant, db
+from flask import Blueprint
+from app.models import Restaurant, db
 
 
-# restaurant_routes = Blueprint('restaurant', __name__)
+restaurant_routes = Blueprint('restaurant', __name__)
+
+# Get all Restaurants
+@restaurant_routes.route('')
+def get_all_restaurants():
+    restaurants = restaurant.query.all()
+    return {'restaurants': [res.to_dict() for res in restaurants]}
+
+# Get Restaurant by ID
+@restaurant_routes.route('/<int:id>')
+def get_restaurant(id):
+    restaurant = restaurant.query.get(id)
+    return {'restaurant': restaurant.to_dict()}
