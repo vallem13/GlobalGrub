2b60b6778abd43dc85ec44b5d4afbec403557476
Makayla Jameson <makayla.jameson01@gmail.com>
2023-08-03 16:38:10 -0500

    fixed create review, still need to finish edit

--- react-app/src/components/Navigation/ProfileButton.js
+++ react-app/src/components/Navigation/ProfileButton.js
@@ -4,11 +4,15 @@ import { logout } from "../../store/session";
 import OpenModalButton from "../OpenModalButton";
 import LoginFormModal from "../LoginFormModal";
 import SignupFormModal from "../SignupFormModal";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import * as sessionActions from '../../store/session';
+
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
   const ulRef = useRef();
+  const history = useHistory()
 
   const openMenu = () => {
     if (showMenu) return;
@@ -29,13 +33,18 @@ function ProfileButton({ user }) {
     return () => document.removeEventListener("click", closeMenu);
   }, [showMenu]);
 
-  const handleLogout = (e) => {
+  const closeMenu = () => setShowMenu(false);
+
+
+  const handleLogout = async (e) => {
     e.preventDefault();
-    dispatch(logout());
+    await dispatch(sessionActions.logout());
+    closeMenu();
+    history.push('/')
+
   };
 
   const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
-  const closeMenu = () => setShowMenu(false);
 
   return (
     <>
--- react-app/src/components/SingleRestaurantDetail/index.js
+++ react-app/src/components/SingleRestaurantDetail/index.js
@@ -44,7 +44,7 @@ export default function SingleRestaurant() {
 
       <div className="reviews-container">
         <div id="post-review-button">
-          <OpenModalButton buttonText="Post Your Review" modalComponent={<CreateReviewModal user={user} restaurant={restaurant} />} />
+          <OpenModalButton buttonText="Post Your Review" modalComponent={<CreateReviewModal user_id={user.id} restaurant={restaurant} />} />
         </div>
 
         {reviews.map((review) => (
@@ -56,7 +56,7 @@ export default function SingleRestaurant() {
             </p>
             <OpenModalButton
               buttonText="Edit Your Review"
-              modalComponent={<EditReviewModal restaurant={restaurant} review={review} user={user} />}
+              modalComponent={<EditReviewModal restaurant={restaurant} review={review} user_id={user.id} />}
             />
 
             <OpenModalButton
