be0ff8cdd35797d68d46f9bb9b51ce2fce6d0749
Alexandra <ale.vallem@gmail.com>
2023-08-04 17:41:50 -0400

    all restaurant cards have the same info

--- react-app/src/components/HomePage/RestaurantCard.js
+++ react-app/src/components/HomePage/RestaurantCard.js
@@ -10,7 +10,7 @@ const RestaurantCard = ({ restaurant }) => {
         <div onClick={onClick}>
             <div>
                 <img src={restaurant.restaurant_image} alt="Restaurant" style={{ width: '400px', height: '400px' }}/>
-                <p> Restaurant: {restaurant.name}</p>
+                <h4>{restaurant.name}</h4>
                 <p>{restaurant.price_range}</p>
                 {restaurant.average_rating !== null ? (
                     <p>{restaurant.average_rating.toFixed(1)}</p>
--- react-app/src/components/Search/filterPrice.js
+++ react-app/src/components/Search/filterPrice.js
@@ -6,15 +6,15 @@ export default function FilterPrice() {
   const history = useHistory();
   const dispatch = useDispatch();
 
-//   const onClick = () => {
-//     history.push(`/restaurant/${restaurant.id}`)
-// }
+  //   const onClick = () => {
+  //     history.push(`/restaurant/${restaurant.id}`)
+  // }
 
   const [searchPrice, setSearchPrice] = useState("");
   const [filteredRestaurant, setFilteredRestaurant] = useState([]);
   const price_range = ["$", "$$", "$$$", "$$$$"];
   const priceRangeImages = {
-    "$": "https://icon-library.com/images/dollar-icon/dollar-icon-23.jpg", 
+    "$": "https://icon-library.com/images/dollar-icon/dollar-icon-23.jpg",
     "$$": "https://icon-library.com/images/dollar-icon/dollar-icon-23.jpg",
     "$$$": "https://icon-library.com/images/dollar-icon/dollar-icon-23.jpg",
     "$$$$": "https://icon-library.com/images/dollar-icon/dollar-icon-23.jpg"
@@ -22,12 +22,12 @@ export default function FilterPrice() {
   };
 
   const FilterPriceThunk = async (priceRange) => {
-   
-      const res = await fetch(`/api/search/restaurants?price_range=${priceRange}`);
-      if (res.ok) {
-        const result = await res.json();
-        setFilteredRestaurant(result.restaurants);
-      } 
+
+    const res = await fetch(`/api/search/restaurants?price_range=${priceRange}`);
+    if (res.ok) {
+      const result = await res.json();
+      setFilteredRestaurant(result.restaurants);
+    }
   };
 
   const priceOnClick = (priceRange) => {
@@ -42,41 +42,47 @@ export default function FilterPrice() {
 
   const onClick = (restaurantId) => {
     history.push(`/restaurant/${restaurantId}`)
-}
+  }
 
   return (
     <div>
       <h2>Filter by Price:</h2>
       {price_range.map((price) => (
         <span key={price} onClick={() => priceOnClick(price)}>
-        <img src={priceRangeImages[price]} alt={price} width="20" height="20" />
+          <img src={priceRangeImages[price]} alt={price} width="20" height="20" />
 
-    
+
 
         </span>
       ))}
-     
-        <div>
-          
-            <div style={{ display: "flex", flexWrap: "wrap" }}>
-              {filteredRestaurant.map((restaurant) => (
-                
-                <div key={restaurant.id} onClick={() => onClick(restaurant.id)} >
-                   {/* <p>{filteredRestaurant.length} Results</p> */}
-                  <h3>{restaurant.name}</h3>
-                  {restaurant.restaurant_image && (
-            <img 
-              src={restaurant.restaurant_image}
-              alt={restaurant.name}
-              style={{ width: "200px", height: "200px" }} 
-            />
-          )}
-                </div>
-              ))}
+
+      <div>
+
+        <div style={{ display: "flex", flexWrap: "wrap" }}>
+          {filteredRestaurant.map((restaurant) => (
+
+            <div key={restaurant.id} onClick={() => onClick(restaurant.id)} >
+              {/* <p>{filteredRestaurant.length} Results</p> */}
+              {restaurant.restaurant_image && (
+                <img
+                  src={restaurant.restaurant_image}
+                  alt={restaurant.name}
+                  style={{ width: "200px", height: "200px" }}
+                />
+              )}
+              <h4>{restaurant.name}</h4>
+              <p>{restaurant.price_range}</p>
+              {restaurant.average_rating !== null ? (
+                <p>{restaurant.average_rating.toFixed(1)}</p>
+              ) : (
+                <p>No Reviews Yet</p>
+              )}
             </div>
-        
+          ))}
         </div>
-     
+
+      </div>
+
     </div>
   );
 }
