8273d537e15ded9218cadef092d0258214b7bedf
7kthr7 <mohamud.kawthar@gmail.com>
2023-08-03 21:30:22 -0500

    flags for cuisines added

--- react-app/src/components/AllCuisines/index.js
+++ react-app/src/components/AllCuisines/index.js
@@ -1,7 +1,18 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getCuisineThunk } from "../../store/cuisine";
-
+import Korean from "./flags/korea.png"
+import Japanese from "./flags/japan.png"
+import Peruvian from "./flags/peru.png"
+import Vietnamese from "./flags/vietnam.png"
+import Mexican from "./flags/mexico.png"
+import Spanish from "./flags/spain.png"
+import French from "./flags/france.png"
+import Mediterranean from "./flags/greece.png"
+import Thai from "./flags/thailand.png"
+import Somali from "./flags/somalia.png"
+import Jamaican from "./flags/jamaica.png"
+import Indian from "./flags/india.png"
 
 
 const CuisineList = () => {
@@ -17,28 +28,19 @@ const CuisineList = () => {
 
 
     const flagImages = {
-        Korean: "https://upload.wikimedia.org/wikipedia/commons/0/09/Flag_of_South_Korea.svg",
-        Japanese: "https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Flag_of_Japan.svg/1024px-Flag_of_Japan.svg.png",
-        Peruvian: "https://upload.wikimedia.org/wikipedia/commons/f/f1/Peru_flag_with_coat_of_arms_300.PNG",
-        Vietnamese: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Vietnam.svg/1200px-Flag_of_Vietnam.svg.png",
-        Mexican: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Flag_of_Mexico.svg/1024px-Flag_of_Mexico.svg.png",
-        Spanish: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Bandera_de_Espa%C3%B1a.svg/1024px-Bandera_de_Espa%C3%B1a.svg.png",
-        French: "https://upload.wikimedia.org/wikipedia/en/c/c3/Flag_of_France.svg",
-        Mediterranean: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_Greece.svg/2000px-Flag_of_Greece.svg.png",
-        Thai: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Flag_of_Thailand.svg/1024px-Flag_of_Thailand.svg.png",
-        Somali: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Flag_of_Somalia.svg/1024px-Flag_of_Somalia.svg.png",
-        Jamaican: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Flag_of_Jamaica.svg/1024px-Flag_of_Jamaica.svg.png",
-        Indian: "https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/1024px-Flag_of_India.svg.png"
-    };
+        Korean, Japanese, Peruvian, Vietnamese, Mexican, Spanish, French, Mediterranean, Thai, Somali, Jamaican, Indian
+    }
+
     return (
         <div >
             <div style={{ display: "flex" }}>
                 {cuisines.map(cuisine => (
-                    <div style={{ padding: "10px", border: "4px solid #F39C12" }}> {cuisine.type}     <img
-                        src={flagImages[cuisine.type]}
-                        alt={cuisine.type}
-                        style={{ width: "100px", height: "100px", marginRight: "5px" }}
-                    />
+                    <div style={{ padding: "10px"}}>
+                        <img
+                            src={flagImages[cuisine.type]}
+                            alt={cuisine.type}
+                            style={{ width: "70px", height: "70px", marginRight: "40px" }}
+                        />
                     </div>
                 ))}
             </div >
