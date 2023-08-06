87e6924f240d4d0e6c077b4bda8dff425b9fdbc5
7kthr7 <mohamud.kawthar@gmail.com>
2023-08-05 22:32:51 -0500

    added filter buttons

--- react-app/src/components/HomePage/RestaurantCard.js
+++ react-app/src/components/HomePage/RestaurantCard.js
@@ -7,19 +7,22 @@ const RestaurantCard = ({ restaurant }) => {
         history.push(`/restaurant/${restaurant.id}`)
     }
     return (
-        <div onClick={onClick}>
-            <div>
-                
-                <img src={restaurant.restaurant_image} alt="Restaurant" style={{ width: '400px', height: '400px' }}/>
-                <h4>{restaurant.name}</h4>
-                <p>{restaurant.price_range}</p>
-                {restaurant.average_rating !== null ? (
-                    <p>{restaurant.average_rating.toFixed(1)}</p>
-                ) : (
-                    <p>No Reviews Yet</p>
-                )}
-            </div>
+        <div className="restaurant-row" onClick={onClick}>
+        <div className="restaurant-card">
+          <img
+            src={restaurant.restaurant_image}
+            alt="Restaurant"
+            style={{ width: '400px', height: '400px' }}
+          />
+          <h4>{restaurant.name}</h4>
+          <p>{restaurant.price_range}</p>
+          {restaurant.average_rating !== null ? (
+            <p>{restaurant.average_rating.toFixed(1)}</p>
+          ) : (
+            <p>No Reviews Yet</p>
+          )}
         </div>
+      </div>
     )
 }
 export default RestaurantCard;
--- react-app/src/components/Search/filterPrice.js
+++ react-app/src/components/Search/filterPrice.js
@@ -2,28 +2,17 @@ import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import RestaurantCard from "../HomePage/RestaurantCard";
+import "./filterPrice.css"; // Make sure to import your CSS file
 
 export default function FilterPrice() {
   const history = useHistory();
   const dispatch = useDispatch();
 
-  //   const onClick = () => {
-  //     history.push(`/restaurant/${restaurant.id}`)
-  // }
-
   const [searchPrice, setSearchPrice] = useState("");
   const [filteredRestaurant, setFilteredRestaurant] = useState([]);
   const price_range = ["$", "$$", "$$$", "$$$$"];
-  const priceRangeImages = {
-    "$": "https://icon-library.com/images/dollar-icon/dollar-icon-23.jpg",
-    "$$": "https://icon-library.com/images/dollar-icon/dollar-icon-23.jpg",
-    "$$$": "https://icon-library.com/images/dollar-icon/dollar-icon-23.jpg",
-    "$$$$": "https://icon-library.com/images/dollar-icon/dollar-icon-23.jpg"
-
-  };
 
   const FilterPriceThunk = async (priceRange) => {
-
     const res = await fetch(`/api/search/restaurants?price_range=${priceRange}`);
     if (res.ok) {
       const result = await res.json();
@@ -42,35 +31,34 @@ export default function FilterPrice() {
   };
 
   const onClick = (restaurantId) => {
-    history.push(`/restaurant/${restaurantId}`)
-  }
+    history.push(`/restaurant/${restaurantId}`);
+  };
 
   return (
-    <div>
+    <div className="filter-container">
       <h2>Filter by Price:</h2>
-      {price_range.map((price) => (
-        <span key={price} onClick={() => priceOnClick(price)}>
-          <img src={priceRangeImages[price]} alt={price} width="20" height="20" />
-
-
-
-        </span>
-      ))}
-     
-        <div>
-          
-            <div style={{ display: "flex", flexWrap: "wrap" }}>
-              {filteredRestaurant.map((restaurant) => (
-                
-                <div key={restaurant.id} onClick={() => onClick(restaurant.id)} >
-                  <RestaurantCard key={restaurant.id} restaurant={restaurant} />
-        
-                </div>
-              ))}
-            </div>
-        
-        </div>
-     
-    </div>
+      <div className="price-buttons">
+        {price_range.map((price) => (
+          <span
+            key={price}
+            className={`price-button ${searchPrice === price ? "active" : ""}`}
+            onClick={() => priceOnClick(price)}
+          >
+            {price}
+          </span>
+        ))}
+      </div>
+      {/* <div className="restaurant-cards"> */}
+        {filteredRestaurant.map((restaurant) => (
+          <div
+            key={restaurant.id}
+            className="restaurant-filter-card"
+            onClick={() => onClick(restaurant.id)}
+          >
+            <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+          </div>
+        ))}
+      </div>
+   
   );
 }
