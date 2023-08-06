c177bfa14f79bec9067a0ba0040164e70bffc3e0
Makayla Jameson <makayla.jameson01@gmail.com>
2023-08-06 00:27:53 -0500

    finsihed dropdown for profile

--- react-app/src/components/Navigation/ProfileButton.js
+++ react-app/src/components/Navigation/ProfileButton.js
@@ -1,3 +1,5 @@
+// ProfileButton.js
+
 import React, { useState, useEffect, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { logout } from "../../store/session";
@@ -7,13 +9,13 @@ import SignupFormModal from "../SignupFormModal";
 import { NavLink, useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import * as sessionActions from '../../store/session';
 import UserProfile from "../ManageProfile";
-
+import './Navigation.css'; // Import the CSS file
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
   const ulRef = useRef(null);
-  const history = useHistory()
+  const history = useHistory();
 
   const openMenu = () => {
     if (showMenu) return;
@@ -34,27 +36,24 @@ function ProfileButton({ user }) {
     return () => document.removeEventListener("click", closeMenu);
   }, [showMenu]);
 
-
-
   const handleLogout = async (e) => {
     e.preventDefault();
     await dispatch(sessionActions.logout());
     closeMenu();
-    history.push('/')
-
+    history.push('/');
   };
 
   const handleManageAccount = async (e) => {
     e.preventDefault();
     closeMenu();
-    history.push('/profile')
-  }
+    history.push('/profile');
+  };
 
   const handleOrders = async (e) => {
     e.preventDefault();
     closeMenu();
-    history.push('/user_orders')
-  }
+    history.push('/user_orders');
+  };
 
   const closeMenu = () => setShowMenu(false);
   const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
@@ -62,24 +61,40 @@ function ProfileButton({ user }) {
   return (
     <div className="profile-button">
       <button onClick={openMenu}>
-        {/* <i className="fas fa-user-circle" /> */}
         <i className="fas fa-bars" style={{ color: "#000000" }} />
-
       </button>
+
+      {/* Conditionally render the overlay */}
+      {showMenu && <div className="overlay" />}
+
+      {/* Render the profile dropdown */}
       <ul className={ulClassName} ref={ulRef}>
         {user ? (
           <>
-            <p>Hi, {user.firstName}</p>
-            <p>{user.email}</p>
-            <p>
-              <button onClick={handleLogout}>Log Out</button>
+            <div className="user-icon">
+              <i class="fa-solid fa-user"></i>
+              <p className="user-name">Hi, {user.firstName}</p>
+            </div>
+            <p className="user-email">{user.email}</p>
+
+
+            <p className="custom-paragraph">
+              <button onClick={handleManageAccount} className="manage-account-button" type="submit">Manage Account</button>
             </p>
-            <p>
-              <button onClick={handleManageAccount} type="submit">Manage Account</button>
+
+            <p className="custom-paragraph">
+              <button onClick={handleOrders} className="orders-button" type="submit">Orders</button>
             </p>
-            <button onClick={handleOrders} type="submit">Orders</button>
-            <p>
+
+            <p className="custom-paragraph">
+              <button onClick={handleLogout} className="logout-button">Log Out</button>
             </p>
+
+            <p className="created-by">
+              Made with love, laughs & tears: Alex Valle, Natalia Ramirez, Kawthar Mohamud, Makayla Jameson
+              <i class="fa-solid fa-heart"></i>
+            </p>
+
           </>
         ) : (
           <>
