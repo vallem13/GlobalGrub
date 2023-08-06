1ea5627e82d921e673d1d281af47b092d5282322
7kthr7 <mohamud.kawthar@gmail.com>
2023-08-05 20:33:13 -0500

    restaurant carousel is functioning

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,35 +1,98 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getAllRestaurantsThunk } from "../../store/restaurant";
 import RestaurantCard from "./RestaurantCard";
-import TopRated from "./TopRated";
+import './HomePage.css';
+import FilterPrice from "../Search/filterPrice";
 
-
-
-const HomePage = () => {
+const RestaurantCarousel = () => {
   const dispatch = useDispatch();
   const restaurants = Object.values(useSelector(state => state.restaurant.allRestaurants));
-
-  
-
   useEffect(() => {
     dispatch(getAllRestaurantsThunk())
   }, [dispatch])
 
-  restaurants.sort((a, b) => b.average_rating - a.average_rating);
+  //sorts through restaurants in decending order
+  const sortRestaurants = Object.values(restaurants).sort((a, b) => b.average_rating - a.average_rating);
+  //selects the top 9
+  const highestRated = sortRestaurants.slice(0, 9);
 
+  //keeps track of the current index of the two courasels
+  const [topRatedIdx, setTopRatedIdx] = useState(0);
+  const [allRestaurantsIdx, setAllRestaurantsIdx] = useState(0);
+  //display 3 restaurants at a time 
+  const itemsPerPage = 3;
 
+  //event handle for next
+  const handleClickNext = (carouselType) => {
+    //passed into handleClickNext 
+    if (carouselType === 'topRated') {
+      //topIdx starts at 0
+      // when clicked next we add 1 and the new index is set to [1] etc..
+      // if topRated has a length of 9 and its starts at 
+      //(0 + 1) % (9-3 +7) 1 % 7 = 7
+      //(1 + 1) % (7)
+      // the highes idx that can be displayed while still having a page of 3
+
+      setTopRatedIdx((topRatedIdx + 1) % (highestRated.length - itemsPerPage + 1));
+    } else if (carouselType === 'allRestaurants') {
+      setAllRestaurantsIdx((allRestaurantsIdx + 1) % (restaurants.length - itemsPerPage + 1));
+    }
+  };
+
+  const handleClickPrev = (carouselType) => {
+    if (carouselType === 'topRated') {
+      setTopRatedIdx((topRatedIdx - 1 + highestRated.length - itemsPerPage + 1) % (highestRated.length - itemsPerPage + 1));
+    } else if (carouselType === 'allRestaurants') {
+      setAllRestaurantsIdx((allRestaurantsIdx - 1 + restaurants.length - itemsPerPage + 1) % (restaurants.length - itemsPerPage + 1));
+    }
+  };
+
+  const displayHighestRatedRestaurants = highestRated.slice(topRatedIdx, topRatedIdx + itemsPerPage);
+  const displayAllRestaurants = restaurants.slice(allRestaurantsIdx, allRestaurantsIdx + itemsPerPage);
 
   return (
     <div>
 
-      {restaurants.map(restaurant => (
-        <RestaurantCard key={restaurant.id} restaurant={restaurant} />
-      ))}
-    
-      <h3>TOP RATED RESTAURANTS</h3>
-      <TopRated restaurants={restaurants} />
-    </div >
+
+      <div className="carousel-wrapper">
+        <h3>Top Rated</h3>
+        <div className="carousel">
+          {displayHighestRatedRestaurants.map(restaurant => (
+            <div key={restaurant.id} className="carousel-item">
+              <RestaurantCard restaurant={restaurant} />
+            </div>
+          ))}
+        </div>
+        <div className="carousel-buttons">
+          <button className="prev-button" onClick={() => handleClickPrev('topRated')}>
+            &lt;
+          </button>
+          <button className="next-button" onClick={() => handleClickNext('topRated')}>
+            &gt;
+          </button>
+        </div>
+      </div>
+      <div className="carousel-wrapper">
+        <h3>All Restaurants</h3>
+        <div className="carousel">
+          {displayAllRestaurants.map(restaurant => (
+            <div key={restaurant.id} className="carousel-item">
+              <RestaurantCard restaurant={restaurant} />
+            </div>
+          ))}
+        </div>
+        <div className="carousel-buttons">
+          <button className="prev-button" onClick={() => handleClickPrev('allRestaurants')}>
+            &lt;
+          </button>
+          <button className="next-button" onClick={() => handleClickNext('allRestaurants')}>
+            &gt;
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
-export default HomePage;
+
+export default RestaurantCarousel;
--- react-app/src/components/RestaurantCarousel/RestaurantImageIndex.js
+++
@@ -1,5 +0,0 @@
-import { Link } from "react-router-dom"
-
-const RestaurantIndexItem = ({ restaurant, interactive = true, small = false }) => {
-
-}
