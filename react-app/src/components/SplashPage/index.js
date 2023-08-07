c00900cbc700e8b964676128b994855bf60d30f3
Alexandra <ale.vallem@gmail.com>
2023-08-07 11:26:10 -0400

    home page logo

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import "./Splash.css";
+import logo from './Logo/globalgrub.png'
 
 function SplashPage() {
   const sessionUser = useSelector((state) => state.session.user);
@@ -10,6 +11,7 @@ function SplashPage() {
   return (
     <div className="splash-wrapper">
         <div className="splash-text-wrapper">
+        <img src={logo} className="home-page-logo" />
             <h1 className="splash-greeting-text">Order food to your door</h1>
         </div>
     </div>
