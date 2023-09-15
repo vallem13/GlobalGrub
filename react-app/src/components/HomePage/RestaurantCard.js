4c6b34c6a9e9f1f36a6d517e0c16efa3d38e5efa
Alexandra <ale.vallem@gmail.com>
2023-09-15 18:36:20 -0400

    layout

--- react-app/src/components/HomePage/RestaurantCard.js
+++ react-app/src/components/HomePage/RestaurantCard.js
@@ -9,21 +9,25 @@ const RestaurantCard = ({ restaurant }) => {
         history.push(`/restaurant/${restaurant.id}`)
     }
     return (
-        
-            <div className='single-restaurant-item-wrapper' onClick={onClick}>
-                
-                <img className='single-restaurant-image'  src={restaurant.restaurant_image} alt="Restaurant"  />
-                        <div className='single-restaurant-rating-name'>
-                        <h3 >{restaurant.name}</h3>
-                        {restaurant.average_rating !== null ? (
-                            <p className='restaurant-average-rating'>{restaurant.average_rating.toFixed(1)}</p>
-                        ) : (
-                            <p>No Reviews Yet</p>
-                        )}
-                    </div>
-                    </div>
-               
-            
+
+        <div className='single-restaurant-item-wrapper' onClick={onClick}>
+
+            <img className='single-restaurant-image' src={restaurant.restaurant_image} alt="Restaurant" />
+            <div className='single-restaurant-rating-name'>
+                <div className='name-price'>
+                    <h3 >{restaurant.name}</h3>
+                    <h4>{restaurant.price_range}</h4>
+                </div>
+                {restaurant.average_rating !== null ? (
+                    <p className='restaurant-average-rating'>{restaurant.average_rating.toFixed(1)}</p>
+
+                ) : (
+                    <p>No Reviews Yet</p>
+                )}
+            </div>
+        </div>
+
+
     )
 }
 export default RestaurantCard;
--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { getAllRestaurantsThunk } from "../../store/restaurant";
-import './HomePage.css';
 import { useHistory } from "react-router-dom";
 import FilterPrice from "../Search/filterPrice";
 import RestaurantCard from "../HomePage/RestaurantCard";
+import './HomePage.css';
 
 
 
@@ -38,64 +38,59 @@ const RestaurantCarousel = () => {
     }
   };
 
-
-
   const displayHighestRatedRestaurants = highestRated.slice(topRatedIdx, topRatedIdx + itemsPerPage);
 
   return (
 
     <div className="home">
-      <div className="carousel-wrapper-filter">
-        <FilterPrice />
-      </div>
-      <div className="both-carousel-wrapper">
-        <div className="carousel-wrapper" >
-          <div className="carousel-button-title">
-            <div className="carousel-title">
-              <h3 >Highest Rated Restaurants</h3>
-            </div>
-            <div className="carousel-icons">
-              <span class="material-symbols-outlined" onClick={() => handleClickPrev('topRated')}>
-                arrow_circle_left
-              </span>
-              <span class="material-symbols-outlined"  onClick={() => handleClickNext('topRated')}>
-                arrow_circle_right
-              </span>
-
-
-            </div>
-          </div>
-          <div className="carousel-top-wrapper" >
-            {displayHighestRatedRestaurants.map(restaurant => (
-              <div
-                key={restaurant.id}
-                className="carousel-item"
-                onClick={() => handleOnClick(restaurant.id)} // Handle click to navigate to restaurant details
-              >
-                <img
-                  src={restaurant.restaurant_image}
-                  alt="Restaurant"
-                // style={{ width: 'auto', height: '150px' }}
-                />
-                <div className="carousel-item-details">
-                  <div className="restaurant-carousel-name">{restaurant.name}</div>
-                  <button className="restaurant-carousel-avg">{restaurant.average_rating.toFixed(1)}</button>
-                </div>
-                </div>
-            ))}
-          </div>
-
+      <div className="filter-carousel">
+        <div className="carousel-wrapper-filter">
+          <FilterPrice />
         </div>
-        <div className="all-restaurant-wrapper" >
-          <h3 >View All Restaurants</h3>
-          <div className='cuisine-restaurants'>
-        {restaurants.map((restaurant) => (
-          <div key={restaurant.id} onClick={() => handleOnClick(restaurant.id)}>
-            <RestaurantCard restaurant={restaurant} />
+        <div className="both-carousel-wrapper">
+          <div className="carousel-wrapper" >
+            <div className="carousel-button-title">
+              <div className="carousel-title">
+                <h3 >Highest Rated Restaurants</h3>
+              </div>
+              <div className="carousel-icons">
+                <span class="material-symbols-outlined" onClick={() => handleClickPrev('topRated')}>
+                  arrow_circle_left
+                </span>
+                <span class="material-symbols-outlined" onClick={() => handleClickNext('topRated')}>
+                  arrow_circle_right
+                </span>
+              </div>
+            </div>
+            <div className="carousel-top-wrapper" >
+              {displayHighestRatedRestaurants.map(restaurant => (
+                <div
+                  key={restaurant.id}
+                  className="carousel-item"
+                  onClick={() => handleOnClick(restaurant.id)} // Handle click to navigate to restaurant details
+                >
+                  <img
+                    src={restaurant.restaurant_image}
+                    alt="Restaurant"
+                  />
+                  <div className="carousel-item-details">
+                    <div className="restaurant-carousel-name">{restaurant.name}</div>
+                    <button className="restaurant-carousel-avg">{restaurant.average_rating.toFixed(1)}</button>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
-        ))}
+        </div>
       </div>
-
+      <div className="all-restaurant-wrapper" >
+        <h3 >View All Restaurants</h3>
+        <div className='cuisine-restaurants'>
+          {restaurants.map((restaurant) => (
+            <div key={restaurant.id} onClick={() => handleOnClick(restaurant.id)}>
+              <RestaurantCard restaurant={restaurant} />
+            </div>
+          ))}
         </div>
       </div>
     </div>
