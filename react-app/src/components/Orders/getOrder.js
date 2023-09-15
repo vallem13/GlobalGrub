bfe1ec747bc3d62941e65d1e8be2e30f67deae94
Natalia Ramirez <natalia3ramirez@gmail.com>
2023-09-14 20:17:49 -0700

    finalized orders

--- react-app/src/components/Orders/getOrder.js
+++ react-app/src/components/Orders/getOrder.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getOrderThunk, getAllOrderCartsThunk } from '../../store/cart';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 import './UsersOrders.css';
 
 export default function GetOrder() {
   const dispatch = useDispatch();
+  const history = useHistory()
   const orders = useSelector((state) => state.cart.orders);
   const all_orders = Object.entries(orders).reverse();
   const user = useSelector((state) => state.session.user);
@@ -18,48 +20,66 @@ export default function GetOrder() {
     return <div>Loading...</div>;
   }
 
-  return (
-    <div className='users-orders-wrapper'>
-      <h1>Past Orders</h1>
-      <div>
-        {all_orders.map(([orderCartId, orderItems]) => (
-          <div className='single-order-list' key={orderCartId}>
-            <h2>{orderItems[0].restaurant_name}</h2>
-            <h3 className='order-number-title'>Order Number: {orderCartId}</h3>
-            <div className='items-in-order'>
-              {Array.isArray(orderItems) ? (
-                orderItems.map((order) => (
-                  <div className='order-list-items' key={order.order_id}>
-                    <img
-                      src={order.menu_item_image}
-                      alt={order.menu_item_name}
-                      style={{
-                        width: '80px',
-                        height: '80px',
-                        
-                      }}
-                    />{' '}
-                    <h4>
-                      {order.menu_item_name} ${Number(order.menu_item_price).toFixed(2)}
-                    </h4>
-                  </div>
-                ))
-              ) : (
-                <p>No order items available for this order.</p>
-              )}
-            </div>
+  function calculateOrderTotal(orderItems) {
+    return orderItems.reduce((total, order) => total + Number(order.menu_item_price), 0);
+  }
 
-            {/* Calculate and display the total for this order */}
-            <h3 className='order-total'>
-              Total: ${calculateOrderTotal(orderItems).toFixed(2)}
-            </h3>
+  const onClick = (restaurantId) => {
+    history.push(`/restaurant/${restaurantId}`);
+};
+
+  return (
+    <div className="manage-restaurants-page">
+      <div className="title-create-container">
+        <h1>Past Orders</h1>
+      </div>
+      <div className="restaurant-cards-buttons-container">
+        {all_orders.map(([orderCartId, orderItems]) => (
+          <div className="single-restaurant-cards-buttons" key={orderCartId}>
+            <div className='single-restaurant-card'>
+              <div className='image-button-container'>
+                <img
+                  className='restaurant-image'
+                  src={orderItems[0].restaurant_image}
+                  alt="Restaurant" />
+              </div>
+              <div className='single-restaurant-info'>
+                <div>
+                  <h4 className='title'>Restaurant: </h4>
+                  <h4>{orderItems[0].restaurant_name}</h4>
+                </div>
+                <div>
+                  <h4 className='title'>Order Number: </h4>
+                  <h4> {orderCartId}</h4>
+                </div>
+                <div>
+                  <h4 className='title'>Order Details: </h4>
+                  <h4> {Array.isArray(orderItems) && (
+                    orderItems.map((order) => (
+                      <div className='order-list-items' key={order.order_id}>
+                        <h4>
+                          {order.menu_item_name} ${Number(order.menu_item_price).toFixed(2)}
+                        </h4>
+                      </div>
+                    ))
+                  )}</h4>
+                </div>
+                <div>
+                  <h4 className='title'>Total: </h4>
+                  <h4> ${calculateOrderTotal(orderItems).toFixed(2)}</h4>
+                </div>
+              </div>
+            </div>
+            <div className="buttons-container">
+            <button onClick={() => onClick(orderItems[0].restaurant_id)}>Review Restaurant</button>
+
+            </div>
           </div>
         ))}
       </div>
+
     </div>
   );
 }
 
-function calculateOrderTotal(orderItems) {
-  return orderItems.reduce((total, order) => total + Number(order.menu_item_price), 0);
-}
+
