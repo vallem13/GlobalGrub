6d242be0612d7cff9d3ab2e94d945d9655c48807
Natalia Ramirez <natalia3ramirez@gmail.com>
2023-09-15 12:37:23 -0700

    added socials

--- react-app/src/components/Navigation/ProfileButton.js
+++ react-app/src/components/Navigation/ProfileButton.js
@@ -90,38 +90,68 @@ function ProfileButton({ user }) {
               <p className="user-name">Hi, {user.firstName}</p>
             </div>
             <p className="user-email">{user.email}</p>
+            <div className='user-options'>
+
+              <p className="custom-paragraph">
+                <button onClick={handleManageAccount} className="manage-account-button user-icon-button" type="submit"><i class="fa-solid fa-address-card" style={{ color: "#000205", }}></i> Manage Account</button>
+              </p>
+
+              <p className="custom-paragraph">
+                <button onClick={handleOrders} className="orders-button" type="submit"><i class="fa-solid fa-receipt" style={{ color: "#000205", }}></i> Orders</button>
+              </p>
+
+              <p className="custom-paragraph">
+                <button onClick={handleManageBusinesses} className="Manage your Businesses"><i class="fa-solid fa-briefcase" style={{ color: "#000205", }}></i> Manage your Businesses</button>
+              </p>
+
+              <p className="custom-paragraph">
+                <button onClick={handleCreateRestaurant} className="Add your Restaurant"><i class="fa-solid fa-building" style={{ color: "#000205", }}></i> Add your Restaurant</button>
+              </p>
 
 
-            <p className="custom-paragraph">
-              <button onClick={handleManageAccount} className="manage-account-button" type="submit">Manage Account</button>
-            </p>
 
-            <p className="custom-paragraph">
-              <button onClick={handleOrders} className="orders-button" type="submit">Orders</button>
-            </p>
+                <p className="custom-paragraph">
+                  <button onClick={handleLogout} className="logout-button">Sign Out</button>
+                </p>
 
-            <p className="custom-paragraph">
-              <button onClick={handleLogout} className="logout-button">Log Out</button>
-            </p>
 
-            <p className="custom-paragraph">
-              <button onClick={handleManageBusinesses} className="Manage your Businesses">Manage your Businesses</button>
-            </p>
+              <div className="created-by">
+                <span>Creators:</span>
 
-            <p className="custom-paragraph">
-              <button onClick={handleCreateRestaurant} className="Add your Restaurant">Add your Restaurant</button>
-            </p>
-
-            <p className="created-by">
-              Made with love, laughs & tears: Alex Valle, Natalia Ramirez, Kawthar Mohamud, Makayla Jameson
-              <i class="fa-solid fa-heart"></i>
-            </p>
-            <ul class="icons">
-              <div>Natalia Ramirez</div>
-					<li><a href="www.linkedin.com/in/natalia-ramirez-750817151" target="_blank" ><i class="fa-brands fa-linkedin"></i></a></li>
-					<li><a href="https://github.com/Natalia3ramirez" target="_blank"><i class="fab fa-github"></i></a></li>
-				</ul>
-
+                <div className="socials">
+                  <span>Alexandra Valle</span>
+                  <div className='socials-icons'>
+                    <a href="www.linkedin.com/in/alexandra-valle-m" target="_blank" ><i class="fa-brands fa-linkedin" style={{ color: "#f00b51", }}></i></a>
+                    <a href="https://github.com/vallem13" target="_blank"><i class="fab fa-github" style={{ color: "#f00b51", }}></i></a>
+                    <a href="https://vallem13.github.io/" target="_blank"><i class="fa-solid fa-book" FontAwesomeIcon style={{ color: "#f00b51", }}></i></a>
+                  </div>
+                </div>
+                <div className="socials">
+                  <span>Makayla Jameson</span>
+                  <div className='socials-icons'>
+                    <a href="https://www.linkedin.com/in/makayla-jameson" target="_blank" ><i class="fa-brands fa-linkedin" style={{ color: "#f00b51", }}></i></a>
+                    <a href="https://github.com/makaylajameson" target="_blank"><i class="fab fa-github" style={{ color: "#f00b51", }}></i></a>
+                    <a href="https://makaylajameson.github.io/" target="_blank"><i class="fa-solid fa-book" FontAwesomeIcon style={{ color: "#f00b51", }}></i></a>
+                  </div>
+                </div>
+                <div className="socials">
+                  <span>Natalia Ramirez</span>
+                  <div className='socials-icons'>
+                    <a href="www.linkedin.com/in/natalia-ramirez-750817151" target="_blank" ><i class="fa-brands fa-linkedin" style={{ color: "#f00b51", }}></i></a>
+                    <a href="https://github.com/Natalia3ramirez" target="_blank"><i class="fab fa-github" style={{ color: "#f00b51", }}></i></a>
+                    <a href="https://Natalia3ramirez.github.io/" target="_blank"><i class="fa-solid fa-book" FontAwesomeIcon style={{ color: "#f00b51", }}></i></a>
+                  </div>
+                </div>
+                <div className="socials">
+                  <span>Kawthar Mohamud</span>
+                  <div className='socials-icons'>
+                    <a href="" target="_blank" ><i class="fa-brands fa-linkedin" style={{ color: "#f00b51", }}></i></a>
+                    <a href="" target="_blank"><i class="fab fa-github" style={{ color: "#f00b51", }}></i></a>
+                    <a href="" target="_blank"><i class="fa-solid fa-book" FontAwesomeIcon style={{ color: "#f00b51", }}></i></a>
+                  </div>
+                </div>
+              </div>
+            </div>
           </>
         ) : (
           <>
